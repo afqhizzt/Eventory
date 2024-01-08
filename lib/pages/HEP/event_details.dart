87baf3beaf3a6dfec2c827a89bc4e31087a3fba8
Nerea24 <102534660+Nerea24@users.noreply.github.com>
2024-01-08 23:23:35 +0800

    insert image poster in file event_details
    i tambah code utk insert image poster of event in event_details

--- lib/pages/HEP/event_details.dart
+++ lib/pages/HEP/event_details.dart
@@ -19,104 +19,110 @@ class ViewEventPage extends StatelessWidget {
         child: Column(
           children: [
             Container(
-              padding: EdgeInsets.only(top: 20.0),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Text(
-                  'Event Details',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+              width: containerWidth,
+              height: containerHeight * 0.6,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30.0),
+                  bottomRight: Radius.circular(30.0),
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30.0),
+                  bottomRight: Radius.circular(30.0),
+                ),
+                child: Image.network(
+                  eventDetails['eventDetails']?['imageUrl'] ?? '',
+                  width: containerWidth,
+                  height: containerHeight * 0.6,
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
             SizedBox(height: 20),
             Container(
+              width: containerWidth,
+              height: containerHeight,
+              padding: EdgeInsets.all(16.0),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50.0),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(50.0),
-                child: Image.network(
-                  eventDetails['eventDetails']?['imageUrl'] ?? '',
-                  width: containerWidth,
-                  height: containerHeight * 0.6,
-                  fit: BoxFit.contain,
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
                 ),
               ),
-            ),
-            SizedBox(height: 40),
-            Center(
-              child: Container(
-                width: containerWidth,
-                height: containerHeight,
-                padding: EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    buildText('Activity Name', 'activityName'),
-                    SizedBox(height: 10),
-                    buildText('Venue', 'venue'),
-                    SizedBox(height: 10),
-                    buildText('Date', 'startDate',
-                        suffix:
-                            ' - ${eventDetails['eventDetails']?['endDate'] ?? ''}'),
-                    SizedBox(height: 10),
-                    buildText('Director', 'director'),
-                    SizedBox(height: 10),
-                    buildText('Category', 'category'),
-                    SizedBox(height: 25),
-                    Container(
-                      alignment: Alignment.center,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Color.fromARGB(255, 2, 81, 19),
-                            ),
-                            child: Text(
-                              'Approved',
-                              style: TextStyle(
-                                color: const Color.fromARGB(255, 255, 255, 255),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                              width:
-                                  20), // Adjust the spacing between buttons as needed
-                          ElevatedButton(
-                            onPressed: () {
-                              // Handle 'Not Approved' action
-                              // You can add logic here to update the status in the database
-                              Navigator.pop(context);
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors
-                                  .red, // Use a color indicating 'Not Approved'
-                            ),
-                            child: Text(
-                              'Not Approved',
-                              style: TextStyle(
-                                color: const Color.fromARGB(255, 255, 255, 255),
-                              ),
-                            ),
-                          ),
-                        ],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      'Event Details',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(height: 20),
+                  buildText('Activity Name', 'activityName'),
+                  SizedBox(height: 10),
+                  buildText('Venue', 'venue'),
+                  SizedBox(height: 10),
+                  buildText('Date', 'startDate',
+                      suffix:
+                          ' - ${eventDetails['eventDetails']?['endDate'] ?? ''}'),
+                  SizedBox(height: 10),
+                  buildText('Director', 'director'),
+                  SizedBox(height: 10),
+                  buildText('Category', 'category'),
+                  SizedBox(height: 25),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromARGB(255, 2, 81, 19),
+                          ),
+                          child: Text(
+                            'Approved',
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                            width:
+                                20), // Adjust the spacing between buttons as needed
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            // Handle 'Not Approved' action
+                            // You can add logic here to update the status in the database
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors
+                                .red, // Use a color indicating 'Not Approved'
+                          ),
+                          child: Text(
+                            'Not Approved',
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
